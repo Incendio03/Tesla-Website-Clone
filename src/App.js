9997b8659b5fb6f768fc6462b44f92ffe3f19ab8
Gemuel Cornillez <gemuel.dev@gmail.com>
2025-12-24 13:49:46 +0800

    Refactor imports and styled-components props
    Cleaned up unused imports in App.js and Header.js. Updated Section.js to use consistent import style and changed styled-components prop from 'bgImg' to '$bgImg' for better prop handling. Improved code formatting and readability.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Section from './components/Section';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
